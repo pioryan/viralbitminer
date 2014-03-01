3d2efd62cba869484f279bb8534cc3b978a60719
Pio Ryan <pioryan@gmail.com>
2014-03-02 04:26:33 +0800

    initi

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Viralbitminer::Application.config.secret_key_base = '1dc0479e1d23dadd6ad1f74f6e0ce79d2d279198e2b5b3233562bb94e4f90f06e72bcd12de99774632b8c2cec3be0d505e245094f6d3cd2f74bf54a1467400c5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Viralbitminer::Application.config.session_store :cookie_store, key: '_viralbitminer_session'
